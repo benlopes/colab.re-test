98a593ff967cb4dbece973fdbb12bcdd06483596
Ruben Lopes <ruben.lopes@syneoshealth.com>
2023-04-12 17:50:05 -0300

    feat: assemble components at home

--- colab/src/pages/index.tsx
+++ colab/src/pages/index.tsx
@@ -1,17 +1,18 @@
-import Image from "next/image";
 import { Inter } from "next/font/google";
 import Head from "next/head";
 import Header from "@/components/Header";
+import Users from "@/components/Users";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between py-12 px-24">
       <Head>
         <title>Users list</title>
       </Head>
       <Header />
+      <Users />
     </main>
   );
 }
